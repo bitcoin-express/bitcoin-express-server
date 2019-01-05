614b59149b6678f2f81f901ac179c82e110c82f6
Przemyslaw Czumaj <bitcoin-express.git@mails.czumaj.pl>
2019-01-05 20:36:05 +0000

    user acount is now storred using Account class and saved in res.params

--- middlewares.js
+++ middlewares.js
@@ -1,8 +1,8 @@
 const db = require('./db');
-const authReqs = [ '/register', '/payment', '/panel', ];
+const config = require('config');
+const { Account } = require(config.get('system.root_dir') + '/core/models/Account');
 
 function sanitiseRequest(req) {
-
     if (req.body) {
         delete req.body._id;
         delete req.body.account_id;
@@ -32,29 +32,16 @@ exports.requireAuthentication = async function (req, res, next) {
 
     try {
         // Try to find account authenticated by the passed token
-        let account = await db.findOne('accounts', {auth_token: req.headers["be-mg-auth-token"]});
+        // let account = await db.findOne('accounts', { auth_token: req.headers["be-mg-auth-token"], });
+        let account = await Account.find(req.headers["be-mg-auth-token"]);
 
-        // If it's there move forward
-        let id = account._id;
-
-        delete account.private_key;
-        delete account.auth_token;
-        delete account.account_id;
-        delete account._id;
-
-        if (req.method === "GET") {
-            req.query.account_id = id;
-            req.query.account = account;
-        }
-        else {
-            req.body.account_id = id;
-            req.body.account = account;
-        }
+        req.params._account_id = account.account_id;
+        req.params._account = account;
 
         return next();
     }
     catch {
-        // In case the account is ni=ot found or somehing will go wrong - quit
+        // In case the account is not found or something went wrong - quit
         return res.status(401).send("No account with this auth token");
     }
 };
@@ -69,4 +56,4 @@ exports.corsMiddleware = function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, BE-MG-Auth-Token");
 
   next();
-}
+};
